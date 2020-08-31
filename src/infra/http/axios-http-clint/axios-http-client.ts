b2b1ead0d5ae5376663253c198cb5ae13cb5667e
Jorge <jlimajorge@gmail.com>
2020-08-31 16:55:20 -0300

    refactor: move SUT to a factory method

--- src/infra/http/axios-http-clint/axios-http-client.ts
+++ src/infra/http/axios-http-clint/axios-http-client.ts
@@ -1,7 +1,7 @@
 import { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient<T, R> {
+export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
     await axios(params.url)
   }
