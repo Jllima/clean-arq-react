42ea2de09c4758d9333802d6dc1e26fe384cfe90
Jorge <jlimajorge@gmail.com>
2020-09-11 11:36:45 -0300

    feat: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -3,6 +3,7 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 
 export const EmailValidation: FieldValidation = {
   validate (field: string, value: string): Error {
-    return new InvalidFieldError(field)
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(value) ? null : new InvalidFieldError(field)
   }
 }
--- src/validation/validators/email/email-validato.spec.ts
+++ src/validation/validators/email/email-validato.spec.ts
@@ -1,10 +1,16 @@
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Shold return error if email is invalid', () => {
     const sut = EmailValidation
-    const error = sut.validate('email', '')
+    const error = sut.validate('email', faker.random.words())
     expect(error).toEqual(new InvalidFieldError('email'))
+  })
+  test('Shold return falsy if email is valid', () => {
+    const sut = EmailValidation
+    const error = sut.validate('email', faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
