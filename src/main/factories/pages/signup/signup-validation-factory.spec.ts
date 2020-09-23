d78ec59bfd75cbe8af6b4761f65e72ef4618f2cf
jorge <jlimajorge@gmail.com>
2020-09-23 12:29:52 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { makeSignUpvalidation } from './signup-validation-factory'
+import { ValidationComposite, ValidationBuilder as Builder } from '@/validation/validators'
+
+describe('LoginValidationFactory', () => {
+  test('Shold make ValidationComposite with correct validations', () => {
+    const sut = makeSignUpvalidation()
+    expect(sut).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
